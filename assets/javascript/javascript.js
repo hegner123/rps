4f989037e8f31c0648b0a4f06f28ba4afcdb118f
Michael Hegner <hegner123@gmail.com>
2019-09-16 14:24:03 -0400

    created files

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,15 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDNiASGDyPT5wj1zAz5Gc55g-wYHcjsG10",
+  authDomain: "hegner123-38bad.firebaseapp.com",
+  databaseURL: "https://hegner123-38bad.firebaseio.com",
+  projectId: "hegner123-38bad",
+  storageBucket: "",
+  messagingSenderId: "385773929972",
+  appId: "1:385773929972:web:916def21d4e2b3007f25a1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+
+
